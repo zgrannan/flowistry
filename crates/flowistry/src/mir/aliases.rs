5a7bcad871f361538bbbc15f27cd72621a794c17
Zack Grannan <zgrannan@protonmail.com>
2025-04-07 10:18:51 -0700

    WIP

--- crates/flowistry/src/mir/aliases.rs
+++ crates/flowistry/src/mir/aliases.rs
@@ -3,7 +3,7 @@
 use std::{hash::Hash, time::Instant};
 
 use log::{debug, info};
-use pcg::{free_pcs::PcgBasicBlocks, run_pcg};
+use pcg::{borrow_pcg::borrow_checker::r#impl::BorrowCheckerImpl, free_pcs::PcgBasicBlocks, run_pcg};
 use rustc_borrowck::consumers::BodyWithBorrowckFacts;
 use rustc_data_structures::{
   fx::{FxHashMap as HashMap, FxHashSet as HashSet},
@@ -78,7 +78,8 @@ impl<'a, 'tcx> Aliases<'a, 'tcx> {
     body_with_facts: &'a BodyWithBorrowckFacts<'tcx>,
   ) -> Self {
     let loans = Self::compute_loans(tcx, def_id, body_with_facts, |_, _, _| true);
-    let pcg_blocks = run_pcg(body_with_facts, tcx, None)
+    let bc = BorrowCheckerImpl::new(tcx, body_with_facts);
+    let pcg_blocks = run_pcg(&body_with_facts.body, tcx, &bc, None)
       .results_for_all_blocks()
       .unwrap();
     Aliases {
@@ -99,7 +100,8 @@ impl<'a, 'tcx> Aliases<'a, 'tcx> {
     selector: impl Fn(RegionVid, RegionVid, BorrowckLocationIndex) -> bool,
   ) -> Self {
     let loans = Self::compute_loans(tcx, def_id, body_with_facts, selector);
-    let pcg_blocks = run_pcg(body_with_facts, tcx, None)
+    let bc = BorrowCheckerImpl::new(tcx, body_with_facts);
+    let pcg_blocks = run_pcg(&body_with_facts.body, tcx, &bc, None)
       .results_for_all_blocks()
       .unwrap();
     Aliases {
